1a15d84dc9d9c7daf0df748436551df80ee6d7d4
Jonno <jonathan.s.bourne.com>
2022-11-13 14:45:06 +0000

    file import experiment. I have put the import commands into the settings.ini file to see if it breaks

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setuptools.setup(
     ] + ['Programming Language :: Python :: '+o for o in py_versions[py_versions.index(min_python):]] + (['License :: ' + lic[1] ] if lic[1] else []),
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
-    include_package_data = True,
-    package_data={'': ['datasets/*.csv']},#manually added
+    #include_package_data = True,
+    #package_data={'': ['datasets/*.csv']},#manually added
     install_requires = requirements,
     extras_require={ 'dev': dev_requirements },
     dependency_links = cfg.get('dep_links','').split(),
