c84712938489b8f47dee6e361104db31c6f85ac7
Jonno <jonathan.s.bourne.com>
2022-11-14 20:50:25 +0000

    tweak to the setup and new dataset functions

--- setup.py
+++ setup.py
@@ -41,8 +41,7 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    package_data={'': ['wind_coef_data/*.csv'],
-                 '': ['example_data/*.csv']},#manually added
+    package_data={"": ["datasets/*.csv", "wind_coef_data/*.csv"]},#manually added
     install_requires = requirements,
     extras_require={ 'dev': dev_requirements },
     dependency_links = cfg.get('dep_links','').split(),
